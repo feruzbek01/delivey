515f6a6f6a576c1e521143781c7dd23f1fd2a947
AlForuq <muhammad.alforuq@gmail.com>
2022-09-12 13:45:42 +0500

    startup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import "bootstrap/dist/css/bootstrap.css";
+// import "remixicon/fonts/remixicon.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
